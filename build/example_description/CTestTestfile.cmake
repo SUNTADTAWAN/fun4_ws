3a0ec496a3fb26410939f46e9635f3d0233dba7c
SUNTADTAWAN <123621257+SUNTADTAWAN@users.noreply.github.com>
2024-09-27 11:39:39 +0700

    create_node for test

---
+++ build/example_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tadtawan/fun4_ws/src
+# Build directory: /home/tadtawan/fun4_ws/build/example_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
