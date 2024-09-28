94d743851c1511fad18885a74bdf24222295cd68
SUNTADTAWAN <123621257+SUNTADTAWAN@users.noreply.github.com>
2024-09-28 15:12:55 +0700

    15:12

--- install/example_description/lib/example_description/controller_node.py
+++ install/example_description/lib/example_description/controller_node.py
@@ -15,7 +15,7 @@ class ControllerNode(Node):
     def __init__(self):
         super().__init__('controller_node')
         self.joint_sub = self.create_subscription(JointState, "/joint_states",self.joint_states_callback,1)
-        self.now_pose_send = self.create_publisher(PoseStamped,'/now_pose',10)
+        self.end_effector_send = self.create_publisher(PoseStamped,'/end_effector',10)
         
         self.joint = [0.0,0.0,0.0]
         
@@ -23,19 +23,17 @@ class ControllerNode(Node):
         L1 = 0.2
         L2 = 0.25
         L3 = 0.15
-        q = [0,-pi/2,0]
-        q[0] += q1
-        q[1] += q2
-        q[2] += q3
+        q = [0+q1,-pi/2+q2,pi/2+q3]
         robot = rtb.DHRobot(
         [
         rtb.RevoluteMDH(a=0,alpha=0,d=L1),
-        rtb.RevoluteMDH(a=0.12,alpha=-pi/2,d=L2),
-        rtb.RevoluteMDH(a=L3,alpha=0,d=0.1),
+        rtb.RevoluteMDH(a=0,alpha=-pi/2,d=-0.12),
+        rtb.RevoluteMDH(a=L2,alpha=0,d=0.1),
         ],
         name = "Robot"
         )
-        robot.tool = SE3.Trans(0.0, 0.0 ,0.13)
+        Q = [0.0,0.0,0.0]
+        robot.tool = SE3.Trans(0.0, -(0.13+L3) ,0.0)
         foward_kinemetic = robot.fkine(q)
 
         msg =  PoseStamped()
@@ -44,13 +42,15 @@ class ControllerNode(Node):
         msg.pose.position.x = foward_kinemetic.t[0]
         msg.pose.position.y = foward_kinemetic.t[1]
         msg.pose.position.z = foward_kinemetic.t[2]
-        self.now_pose_send.publish(msg)
+        self.end_effector_send.publish(msg)
+
 
     def joint_states_callback(self,msg:JointState):
         self.joint[0] = msg.position[0]
         self.joint[1] = msg.position[1]
         self.joint[2] = msg.position[2]
         self.dh_parameter(self.joint[0],self.joint[1],self.joint[2])
+        
 
         
 
